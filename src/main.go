895af101350f7d2e42ac2625f8bb79398132ac50
furikake6000 <ewave63@keio.jp>
2019-08-12 18:26:39 +0900

    簡単なサーバーを走らせること

--- src/main.go
+++ src/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello world\n")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+			<html>
+        <head>
+          <title>Golang web testpage</title>
+        </head>
+        <body>
+          hello world!
+        </body>
+      </html>
+		`))
+  })
+  
+  // Webサーバーを開始
+  err := http.ListenAndServe(":8080", nil)
+  if err != nil {
+    log.Fatal("ListenAndServe:", err)
+  }
 }
