d1b541ba5fc61575ccf28c836d76ec323fd9f21e
furikake6000 <ewave63@keio.jp>
2019-08-13 00:23:11 +0900

    MySQLデータベースとの接続・Userテーブルの作成まで

--- app/main.go
+++ app/main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin"
+  "github.com/jinzhu/gorm"
+  _ "github.com/go-sql-driver/mysql"
 )
+
+type User struct {
+  gorm.Model
+  Name string
+  Age int
+}
 
 func main() {
   router := gin.Default()
+  var db *gorm.DB
+  for {
+    var err error
+    db, err = gorm.Open("mysql", "root:mysql@tcp(db)/golang_webapp")
+
+    // 接続成功するまでポーリング
+    if err == nil {
+      break
+    }
+  }
   router.LoadHTMLGlob("templates/*.html")
 
   router.GET("/", func(cont *gin.Context) {
     cont.HTML(200, "index.html", gin.H{})
   })
+  
+  db.AutoMigrate(&User{})
 
   router.Run(":8080")
+
+  defer db.Close()
 }
