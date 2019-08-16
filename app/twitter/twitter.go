f90b226e8b18341caa771fc854c52849cd7ad5d9
furikake6000 <ewave63@keio.jp>
2019-08-16 23:48:52 +0900

    Twitter APIのURL群をグローバルに

--- app/twitter/twitter.go
+++ app/twitter/twitter.go
@@ -6,17 +6,17 @@ import (
 )
 
 const (
-	refreshTokenURL  = "https://api.twitter.com/oauth/request_token"
-	authorizationURL = "https://api.twitter.com/oauth/authenticate"
-	accessTokenURL   = "https://api.twitter.com/oauth/access_token"
-	accountURL       = "https://api.twitter.com/1.1/account/verify_credentials.json"
+	RefreshTokenURL  = "https://api.twitter.com/oauth/request_token"
+	AuthorizationURL = "https://api.twitter.com/oauth/authenticate"
+	AccessTokenURL   = "https://api.twitter.com/oauth/access_token"
+	AccountURL       = "https://api.twitter.com/1.1/account/verify_credentials.json"
 )
 
 func Client() *oauth.Client {
 	newClient := &oauth.Client{
-		TemporaryCredentialRequestURI: refreshTokenURL,
-		ResourceOwnerAuthorizationURI: authorizationURL,
-		TokenRequestURI:               accessTokenURL,
+		TemporaryCredentialRequestURI: RefreshTokenURL,
+		ResourceOwnerAuthorizationURI: AuthorizationURL,
+		TokenRequestURI:               AccessTokenURL,
 		Credentials: oauth.Credentials{
 			Token:  secret.Credentials["twitter_key"],
 			Secret: secret.Credentials["twitter_secret"],
