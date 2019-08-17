a7112404402593e8c45a90af51e636b8d17c1a89
furikake6000 <ewave63@keio.jp>
2019-08-18 06:39:23 +0900

    Userモデルの属性ミス修正

--- app/models/user.go
+++ app/models/user.go
@@ -6,8 +6,8 @@ import (
 )
 
 type User struct {
-	gorm.Model `gorm:"unique;not null"`
-	TwitterId  string
+	gorm.Model
+	TwitterId  string `gorm:"unique;not null"`
 	ScreenName string
 	ImageURL   string
 }
